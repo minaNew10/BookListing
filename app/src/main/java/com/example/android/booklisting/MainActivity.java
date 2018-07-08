a521c76bc6394eeaa52c7ff91caef3bc738aee42
mina isaac <mina2011new@gmail.com>
2018-07-08 16:38:37 +0200

    create main activity xml

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // api key = AIzaSyCNkAmJ9fPmCvC4mPd8YfMpdKGB1cK7um8
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
